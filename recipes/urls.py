638d865378942dc38f74e8062246d8e3abd67f77
Vinícius <viniciusandradebc@gmail.com>
2025-08-09 22:37:44 -0300

    Making dinamic links

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,9 +3,10 @@ from django.urls import path, re_path
 from recipes.views import home
 from . import views
 
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 
 ]
